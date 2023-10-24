09a88daf57c3a294383869bebfaad851637c1ed5
phette23 <phette23@gmail.com>
2023-10-24 09:35:51 -0700

    update app, general structure outline
    get courses by category
    pass category & ignored courses in .env
    get feedbacks from multiple courses
    still needs work from here for a demo

--- app.py
+++ app.py
@@ -6,11 +6,53 @@ from requests import get, HTTPError
 
 # a tripartite set of API calls:
 # 1. get all courses in the Internships category (may need to weed out demo/template courses)
-# wsfunction: TBD
+# wsfunction: core_course_get_courses_by_field
 # 2. get all Feedback activities from courses (1 request, no need for iteration)
 # wsfunction: mod_feedback_get_feedbacks_by_courses
 # 3. iterate over feedbacks, get all their analyses
 # wsfunction: mod_feedback_get_analysis
+
+
+conf = {
+    **dotenv_values(".env"),  # load private env
+    **os.environ,  # override loaded values with environment variables
+}
+
+
+def debug(s):
+    """print message only if DEBUG env var is True
+
+    Args:
+        s (str): message to print
+    """
+    if conf.get("DEBUG") and bool(conf["DEBUG"]):
+        print(s)
+
+
+# 1 get courses
+def get_courses():
+    service = "core_course_get_courses_by_field"
+    format = "json"
+    params = {
+        # see https://moodle.cca.edu/admin/settings.php?section=webservicetokens
+        "wstoken": conf["TOKEN"],
+        "wsfunction": service,
+        "moodlewsrestformat": format,
+        "field": "category",
+        "value": conf["CATEGORY"],
+    }
+
+    response = get(conf["URL"], params=params)
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        print(f"HTTP Error {response.status_code}")
+        print(response.headers)
+        print(response.text)
+
+    data = response.json()
+    # ? does it matter if we return full course objects or just IDs?
+    return data["courses"]
 
 
 def write_csv(feedback, id):
@@ -20,7 +62,7 @@ def write_csv(feedback, id):
         return
 
     filename = f"data/{id}-feedback.csv"
-    # extract column names from first response
+    # extract column names from first response, see get_feedbacks for structure info
     column_labels = [
         response["name"] for response in feedback["attempts"][0]["responses"]
     ]
@@ -35,61 +77,44 @@ def write_csv(feedback, id):
     print(f"Wrote {filename}")
 
 
-conf = {
-    **dotenv_values(".env"),  # load private env
-    **os.environ,  # override loaded values with environment variables
-}
+def course_ids(courses):
+    """return list of course ids from list of courses, skip ignored courses
+
+    Args:
+        courses (list[dict]): list of course dicts with at least an "id" property
+
+    Returns:
+        list[str]: list of course ids as strings
+    """
+    ids = [
+        str(c["id"])
+        for c in courses
+        if str(c["id"]) not in conf["IGNORED_COURSES"].split(",")
+    ]
+    return ids
+
 
 # 2: get feedbacks
-service = "mod_feedback_get_feedbacks_by_courses"
-format = "json"
-params = {
-    # see https://moodle.cca.edu/admin/settings.php?section=webservicetokens
-    "wstoken": conf["TOKEN"],
-    "wsfunction": service,
-    "moodlewsrestformat": format,
-    "courseids[]": 5204,
-}
+def get_feedbacks(courses):
+    ids = course_ids(courses)
 
-response = get(conf["URL"], params=params)
-try:
-    response.raise_for_status()
-except HTTPError:
-    print(f"HTTP Error {response.status_code}")
-    print(response.headers)
-    print(response.text)
-
-data = response.json()
-feedbacks = data["feedbacks"]
-# example feedback structure:
-# {
-#   "id": 1520,
-#   "course": 5204,
-#   "name": "Submit Employer and Intern Information",
-#   "intro": "",
-#   "introformat": 1,
-#   "anonymous": 2,
-#   "multiple_submit": false,
-#   "autonumbering": false,
-#   "page_after_submitformat": 1,
-#   "publish_stats": false,
-#   "completionsubmit": true,
-#   "coursemodule": 239207,
-#   "introfiles": []
-# }
-
-# 3: get analyses
-# @TODO we do not need _all_ feedbacks only employer info & student evaluation ones
-for fdbk in feedbacks:
-    # see note in readme about the difference between these 2 functions
-    service = "mod_feedback_get_responses_analysis"
-    # service = 'mod_feedback_get_analysis'
+    service = "mod_feedback_get_feedbacks_by_courses"
+    format = "json"
+    # ! this doesn't work, "Access control exception", but my token has all the necessary permissions
+    # ! maybe because of the courseids param formatting?
     params = {
+        # see https://moodle.cca.edu/admin/settings.php?section=webservicetokens
         "wstoken": conf["TOKEN"],
         "wsfunction": service,
         "moodlewsrestformat": format,
-        "feedbackid": fdbk["id"],
+        "courseids[]": ",".join(ids),
     }
+    # each course id is its own URL parameter, weird PHP behavior
+    # ! there is probably a potential bug here where many courses -> too long URL
+    # ! URLs can be 2048 chars and getting 2 courses is only 200 so maybe not
+    for idx, id in enumerate(ids):
+        params[f"courseids[${idx}]"] = id
+
     response = get(conf["URL"], params=params)
     try:
         response.raise_for_status()
@@ -99,27 +124,79 @@ for fdbk in feedbacks:
         print(response.text)
 
     data = response.json()
-    # example analysis structure:
+    feedbacks = data["feedbacks"]
+    # example feedback structure:
     # {
-    #   "attempts": [
-    #     {
-    #       "id": 5817,
-    #       "courseid": 0,
-    #       "userid": 11,
-    #       "timemodified": 1683236827,
-    #       "fullname": "Rey .",
-    #       "responses": [
-    #         {
-    #           "id": 10490,
-    #           "name": "Your Phone Number",
-    #           "printval": "323-123-9876",
-    #           "rawval": "323-123-9876"
-    #         },
-    #   ...objects for each question, below is end of "attempts" array
-    #   ],
-    #   "totalattempts": 1,
-    #   "anonattempts": [],
-    #   "totalanonattempts": 0,
-    #   "warnings": []
+    #   "id": 1520,
+    #   "course": 5204,
+    #   "name": "Submit Employer and Intern Information",
+    #   "intro": "",
+    #   "introformat": 1,
+    #   "anonymous": 2,
+    #   "multiple_submit": false,
+    #   "autonumbering": false,
+    #   "page_after_submitformat": 1,
+    #   "publish_stats": false,
+    #   "completionsubmit": true,
+    #   "coursemodule": 239207,
+    #   "introfiles": []
     # }
-    write_csv(data, fdbk["id"])
+    return feedbacks
+
+
+# 3: get analyses
+def get_responses(feedbacks):
+    # TODO we do not need _all_ feedbacks only employer info & student evaluation ones
+    for fdbk in feedbacks:
+        # see note in readme about the difference between these 2 functions
+        service = "mod_feedback_get_responses_analysis"
+        # service = 'mod_feedback_get_analysis'
+        params = {
+            "wstoken": conf["TOKEN"],
+            "wsfunction": service,
+            "moodlewsrestformat": format,
+            "feedbackid": fdbk["id"],
+        }
+        response = get(conf["URL"], params=params)
+        try:
+            response.raise_for_status()
+        except HTTPError:
+            print(f"HTTP Error {response.status_code}")
+            print(response.headers)
+            print(response.text)
+
+        data = response.json()
+        # TODO handle warnings array & check for its presence in other wsfunction data
+        # example analysis structure:
+        # {
+        #   "attempts": [
+        #     {
+        #       "id": 5817,
+        #       "courseid": 0,
+        #       "userid": 11,
+        #       "timemodified": 1683236827,
+        #       "fullname": "Rey .",
+        #       "responses": [
+        #         {
+        #           "id": 10490,
+        #           "name": "Your Phone Number",
+        #           "printval": "323-123-9876",
+        #           "rawval": "323-123-9876"
+        #         },
+        #   ...objects for each question, below is end of "attempts" array
+        #   ],
+        #   "totalattempts": 1,
+        #   "anonattempts": [],
+        #   "totalanonattempts": 0,
+        #   "warnings": []
+        # }
+        # TODO return feedback, don't write csv
+        if len(data["attempts"]) > 0:
+            write_csv(data, fdbk["id"])
+
+
+if __name__ == "__main__":
+    courses = get_courses()
+    feedbacks = get_feedbacks(courses)
+    responses = get_responses(feedbacks)
+    # ? how do we combine responses? hydrate each response with link to course/feedback it comes from?
--- readme.md
+++ readme.md
@@ -13,16 +13,19 @@ See Moodle's [Web Services Overview](https://moodle.cca.edu/admin/settings.php?s
   - **Required capability**: `mod/feedback:viewreports`
 - Add all necessary web service functions to the service (basically, every REST API endpoint the script calls)
   - There's a list of functions in the [API Documentation](https://moodle.cca.edu/admin/webservice/documentation.php)
-  - @TODO function for category get courses
+  - `core_course_get_courses_by_field`
   - `mod_feedback_get_feedbacks_by_courses`
   - `mod_feedback_get_responses_analysis`
 - Add a site admin user as an authorized user of the service
 - [Create a token](https://moodle.cca.edu/admin/webservice/tokens.php?action=create) for the user, select the service, optionally set an expiration date if prudent. You may need to jump to the final page of tokens to see the one you just created.
 - Copy example.env to .env and paste the token value into it
+- Add the category ID that contains the courses with feedbacks and any courses in that category you want to ignore
 
 ## Two WS Functions
 
 Only one of `mod_feedback_get_analysis` or `mod_feedback_get_responses_analysis` is needed; I have opted for `get_responses_analysis`. The former gives more information about how the questions are structured and then puts all responses in a `data` array under the question with little context, while the latter is more focused on the response data, collecting all attempts into an array where each response contains the name of the field and two different values ("print" versus "raw"). Another notable difference: `get_analysis` doesn't actually link back to the user who submitted, while `get_responses_analysis` does (it gives us their ID and full name). We could chain the user id into a user profile API call to fill in more information about them.
+
+TODO: now that we are using anonymous feedbacks this second statement might not be true anymore
 
 ## LICENSE
 
